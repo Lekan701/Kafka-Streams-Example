e28b49c239daa18acab157841db4b5818492b897
Lekan701 <olalekan.busari@outlook.com>
2024-01-25 12:24:30 +0000

    Created project containing regular kafka producer, streamer and consumers using kafka streams

---
+++ domain-service/src/test/java/com/lekan/domainservice/DomainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lekan.domainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
